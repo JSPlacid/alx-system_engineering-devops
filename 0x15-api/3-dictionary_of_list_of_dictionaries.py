899304a542006d3afef9e42a785d7568e3cf3475
JSPlacid <ojoolusegun17@gmail.com>
2023-04-24 14:41:04 -0700

    get employee todo

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,31 +1,36 @@
 #!/usr/bin/python3
-"""Accessing a REST API for todo lists of employees"""
-
+"""
+Extend the python script from exercise 0 to export data in JSON format.
+Records all completed tasks from all employees.
+Format must be: {"USER_ID": [ {"task": TASK_TITLE,
+                               "completed": TASK_COMPLETED_STATUS,
+                               "username": USERNAME"},
+                               {...}
+                            ]
+File name must be: todo_all_employees.json
+"""
 import json
 import requests
-import sys
 
+if __name__ == "__main__":
+    users = requests.get("http://jsonplaceholder.typicode.com/users").json()
+    tasks = requests.get("http://jsonplaceholder.typicode.com/todos").json()
+    storage = {}
 
-if __name__ == '__main__':
-    url = "https://jsonplaceholder.typicode.com/users"
-
-    response = requests.get(url)
-    users = response.json()
-
-    dictionary = {}
     for user in users:
-        user_id = user.get('id')
-        username = user.get('username')
-        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-        url = url + '/todos/'
-        response = requests.get(url)
-        tasks = response.json()
-        dictionary[user_id] = []
+        eid = user.get("id")
+        username = user.get("username")
+        all_tasks = []
+
         for task in tasks:
-            dictionary[user_id].append({
-                "task": task.get('title'),
-                "completed": task.get('completed'),
-                "username": username
-            })
-    with open('todo_all_employees.json', 'w') as file:
-        dump(dictionary, file)
+            if (task.get("userId") == eid and task.get("completed")):
+                temp = {}
+                temp["task"] = task.get("title")
+                temp["completed"] = task.get("completed")
+                temp["username"] = username
+                all_tasks.append(temp)
+
+        storage[eid] = all_tasks
+
+    with open("todo_all_employees.json", 'w+') as jsonfile:
+        json.dump(storage, jsonfile)
